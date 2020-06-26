8f2f78fdf1a4bb1d6a7f883d2a84cbd83b1628d5
Azael Martinez <azael950620@gmail.com>
2020-06-26 17:36:09 -0500

    Change  AccessControllAllowOrigin

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", `${req.headers.origin}`);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
