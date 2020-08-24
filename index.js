8bc03772481a3f6c69c7f38dc1b1857e9f210a27
Azael Martinez <azael950620@gmail.com>
2020-08-24 15:09:21 -0500

    Cambio de configuracion auth0

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const auth0 = initAuth0({
     "X3nEk2NO2lYUBLkgu2v1r_8uatuH-eqESKRP-Q04Qhptvk445mlH3ZaZdNIxjpaf",
   scope: "openid profile",
   audience: "https://Pro035",
-  redirectUri: "http://localhost:3000/api/callback",
-  postLogoutRedirectUri: "http://localhost:3000",
+  redirectUri: "https://pro035.itcomsoft.vercel.app/api/callback",
+  postLogoutRedirectUri: "https://pro035.itcomsoft.vercel.app/",
   session: {
     // The secret used to encrypt the cookie.
     cookieSecret:
@@ -26,11 +26,11 @@ const auth0 = initAuth0({
     // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
     cookieSameSite: "lax",
     // (Optional) Store the id_token in the session. Defaults to false.
-    storeIdToken: false,
+    storeIdToken: true,
     // (Optional) Store the access_token in the session. Defaults to false.
     storeAccessToken: true,
     // (Optional) Store the refresh_token in the session. Defaults to false.
-    storeRefreshToken: false,
+    storeRefreshToken: true,
   },
   oidcClient: {
     // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
