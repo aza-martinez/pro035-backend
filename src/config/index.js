d422354c269cc2c04d849a34accb783c5c8bd10a
Azael Martinez <azael950620@gmail.com>
2021-01-25 16:46:02 -0600

    [ADD] configuration file

--- src/config/index.js
+++ src/config/index.js
@@ -5,4 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 module.exports = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
+  cors: {
+    origin: process.env.DOMAIN_CORS_ORIGIN || "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    methods: "POST,GET,OPTIONS",
+    allowedHeaders: ["authorization", "Content-Type"],
+  },
 };
