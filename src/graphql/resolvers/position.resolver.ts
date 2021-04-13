f0fd90fa9319f99b7e7ef602f8fd0ac7080282c5
Azael Martinez <azael950620@gmail.com>
2021-04-13 13:58:36 -0500

    [FEAT]: Resolvers puestos de trabajo
    - Resolver para obtener puesto de trabajo por empresa
    - Resolver para actualizar puesto de trabajo
    - Resolver para registrar puesto de trabajo.
    
    closes #13, closes #14

--- src/graphql/resolvers/position.resolver.ts
+++ src/graphql/resolvers/position.resolver.ts
@@ -1,7 +1,8 @@
-import { Arg, Authorized, Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Puesto } from "../../entities/position.entitie";
 import { Context } from "../../interfaces/context.interface";
 import { PositionService } from "../../services/position.service";
+import { PositionInput, PositionUpdateInput } from "../types/position.input";
 
 @Resolver()
 export class PositionResolver {
@@ -17,7 +18,39 @@ export class PositionResolver {
     @Arg("company") company: string,
     @Ctx() { user }: Context
   ): Promise<Puesto[]> {
-    console.log(user.cid)
+    console.log(user.cid);
     return await this.#positionService.getAllByCompany(company, user.cid);
+  }
+
+  @Authorized("Administrador")
+  @Query((_returns) => Puesto, { nullable: true })
+  async positionByCompany(
+    @Arg("position") positionId: string,
+    @Arg("company") company: string,
+    @Ctx() { user }: Context
+  ): Promise<Puesto> {
+    return await this.#positionService.get(positionId, company, user.cid);
+  }
+
+  @Authorized("Administrador")
+  @Mutation((_returns) => Puesto, { nullable: true })
+  async createPosition(
+    @Arg("input") input: PositionInput,
+    @Ctx() { user }: Context
+  ): Promise<Puesto> {
+    return await this.#positionService.create({
+      ...input,
+      cliente: user.cid,
+    });
+  }
+
+  @Authorized("Administrador")
+  @Mutation((_returns) => Puesto, { nullable: true })
+  async updatePosition(
+    @Arg("id") positionId: String,
+    @Arg("input") input: PositionUpdateInput,
+    @Ctx() { user }: Context
+  ): Promise<Puesto> {
+    return await this.#positionService.update(positionId, input, user.cid);
   }
 }
---
+++ src/graphql/types/position.input.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class PositionInput {
+  @Field()
+  nombre: String;
+
+  @Field()
+  descripcion: String;
+
+  @Field({ nullable: false })
+  empresa: String;
+}
+
+@InputType()
+export class PositionUpdateInput {
+  @Field()
+  nombre: String;
+
+  @Field()
+  descripcion: String;
+}
